0061e6a1d31e76d85f9a7f680ccca8a2d78cc4fe
Dominic Schumerth <dschu215@gmail.com>
2023-02-23 08:24:32 -0600

    API and Database pruned

--- api/src/main/java/com/dschumerth/api/api/CatService.kt
+++ api/src/main/java/com/dschumerth/api/api/CatService.kt
@@ -2,20 +2,8 @@ package com.dschumerth.api.api
 
 import okhttp3.ResponseBody
 import retrofit2.http.GET
-import retrofit2.http.Query
 
 interface CatService {
     @GET("c")
     suspend fun getImage(): ResponseBody
-
-    @GET("cat/gif")
-    suspend fun getGif(): ResponseBody
-
-    @GET("cat/")
-    suspend fun getCat(
-        @Query("id") id: Int
-    ): ResponseBody
-
-    @GET("api/cats?tags=tag1,tag2&skip=0&limit=10")
-    suspend fun getCats(): ResponseBody
 }
--- database/src/main/java/com/dschumerth/database/daos/CatDao.kt
+++ database/src/main/java/com/dschumerth/database/daos/CatDao.kt
@@ -8,8 +8,6 @@ import com.dschumerth.abstractions.CatPhoto
 
 @Dao
 abstract class CatDao {
-    @Query("SELECT * FROM cat_photos ORDER BY id DESC LIMIT :limit OFFSET :offset")
-    abstract suspend fun getCatsFromDB(limit: Int, offset: Int): List<CatPhoto>
 
     @Query("SELECT * FROM cat_photos ORDER BY id DESC")
     abstract suspend fun getCatsFromDB(): List<CatPhoto>
